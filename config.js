cd0f4a23dcd15664f63aedf332a73a16c4e67afa
luffy <kosseyla.hamouche@gmail.com>
2019-06-05 00:46:51 +0200

    index + c onfig

---
+++ config.js
@@ -0,0 +1,11 @@
+((document) => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyAH0oncQi3cXnqxpwI4pHtqsTDCooqroRc",
+        authDomain: "my-project-bf769.firebaseapp.com",
+        databaseURL: "https://my-project-bf769.firebaseio.com",
+        projectId: "my-project-bf769",
+        storageBucket: "my-project-bf769.appspot.com",
+        messagingSenderId: "975375538780",
+        appId: "1:975375538780:web:ca0f8aefd0fbc653"
+       };
+ })(document);
